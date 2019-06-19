c77de98e2cfd445a5c3a22efdc633a7b0e8da40a
DanielSameh <danielsameh35@gmail.com>
2019-06-19 02:30:30 +0200

    Adding Random To JS

--- script.js
+++ script.js
@@ -9,7 +9,35 @@ var colors=
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = pickcolor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+
+
+colorDisplay.textContent = pickedColor; 
 
 for (var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+
+    
+    squares[i].addEventListener("click", function(){
+        var clickedcolor = this.style.backgroundColor;
+        if(clickedcolor === pickedColor){
+        changeColors(clickedcolor)
+        messageDisplay.textContent="Correct!";
+        }
+        else
+        { this.style.backgroundColor = "#232323" ;
+        messageDisplay.textContent="Try Again!!"; }  
+    });
+}
+
+function changeColors (color){
+    for (var i =0; i <squares.length;i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+function pickcolor (){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
