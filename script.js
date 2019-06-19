e8f47fe9b6d5c4b2c2137078be32491f4941a3f8
DanielSameh <danielsameh35@gmail.com>
2019-06-19 03:06:51 +0200

    Adding New Color Button

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-var colors=
-[
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors= generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickcolor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var headerColor = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    colors= generateRandomColors(6);
+    pickedColor = pickcolor();
+    colorDisplay.textContent = pickedColor; 
+    headerColor.style.backgroundColor = "#232323";
+    resetButton.textContent = "New Color";
+    for (var i = 0; i < squares.length;i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    
+    
+    
+    
+    
+    
+})
 
 
 colorDisplay.textContent = pickedColor; 
@@ -25,6 +36,8 @@ for (var i = 0; i < squares.length; i++){
         if(clickedcolor === pickedColor){
         changeColors(clickedcolor)
         messageDisplay.textContent="Correct!";
+        headerColor.style.backgroundColor = pickedColor;
+        resetButton.textContent = "Try Again?";
         }
         else
         { this.style.backgroundColor = "#232323" ;
@@ -40,4 +53,13 @@ function changeColors (color){
 function pickcolor (){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+function generateRandomColors (num){
+    var arr = []
+
+    for ( var i = 0; i <num ; i++){
+        arr[i] = "rgb("+ Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) +", " + Math.floor(Math.random() * 255) +")";
+    }
+
+    return arr;
 }
